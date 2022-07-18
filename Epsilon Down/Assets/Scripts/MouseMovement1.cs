6c80cc4d091f63cd8b864fb9bf7bf7c9b297f19f
zesty <81732838+zygradeA@users.noreply.github.com>
2022-07-18 13:37:51 +0800

    The Game

--- Epsilon Down/Assets/Scripts/MouseMovement1.cs
+++ Epsilon Down/Assets/Scripts/MouseMovement1.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseMovement : MonoBehaviour
+public class MouseMovement1 : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
-
-    public Transform playerBody;
-
+    public Transform playerbody;
     float xRotation = 0f;
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,13 +15,13 @@ public class MouseMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        playerbody.Rotate(Vector3.up*mouseX);
     }
 }
